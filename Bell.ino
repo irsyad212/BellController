e41f381feeb76d011e046964a768a4e872874568
Irsyad <irsyadmazrimu@gmail.com>
2025-01-21 11:25:34 +0000

    Update Bell.ino

--- Bell.ino
+++ Bell.ino
@@ -6,54 +6,6 @@
 void setup() {
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
-  digitalWrite(A, 1);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 1);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 1);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 1);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 1);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 1);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 1);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 1);
-  delay(DELAY/2);
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  delay(DELAY/2);
 }
 
 void loop() {
